c5df2b3c0f6e978de018f7c9c4b4895d80cf969e
githubjazon <jason.sevenants@gmail.com>
2023-05-25 16:15:13 +0200

    25mai16h15

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,12 +9,33 @@ use App\Entity\Plat;
 use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
         $Categorie = new Categorie();
+
+        $Commande = new Commande();
+
+        $Detail = new Detail();
+
+        $Plat = new Plat();
+        
+        $Utilisateur = new Utilisateur();
+        
 
 
 
